724e03cf6be88d5bbf0afd9b30cb072319dc962f
hathix <neelmehta@college.harvard.edu>
2015-02-15 20:27:24 -0500

    Added Angular, Ace, Bootstrap

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,11 +1,27 @@
 'use strict';
 
-angular.module('jsmintApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [];
+angular.module('jsmintApp').controller('MainCtrl', function($scope, $http) {
+  // ace editor
+  var editor;
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
+  // things to expose to webpage
+  $scope.temp = 55;
+
+  // Runs the selected test on the user's inputted code.
+  $scope.check = function() {
+    var text = editor.getValue();
+
+    // run through Acorn
+    $http.post("/api/jsmint/acorn", { text: text }).success(function(data, status, headers, config){
+        $scope.temp = JSON.stringify(data.result);
     });
+  };
 
-  });
+  // initialize ace editor
+  editor = ace.edit("editor");
+  editor.setTheme("ace/theme/xcode");
+  editor.getSession().setUseWrapMode(true);
+  editor.getSession().setMode("ace/mode/javascript");
+
+  editor.on('change', _.throttle($scope.check, 100));
+});
